0a398a2c7f60a208d4f530cfee693dd6dec31671
vajrasami <vajrsami@gmail.com>
2024-02-04 22:44:08 +0545

    Chat App added

---
+++ server.js
@@ -0,0 +1,30 @@
+//importing modules
+const express = require("express");
+// const dotenv = require("dotenv").config();
+const app = express();
+const http = require("http").createServer(app);
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+//midleware
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/img"));
+
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + "/index.html");
+});
+
+//socket
+const io = require('socket.io')(http);
+
+io.on('connection',(socket)=>{
+    console.log("Connected");
+    socket.on('message', (msg)=> {
+        // console.log(msg);
+        socket.broadcast.emit("message", msg);
+    });
+});
